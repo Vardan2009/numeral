e823c0e6615fd257278eb12ff1571c3965154aa0
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2024-08-30 23:41:41 +0400

    continue if input is empty check

--- numeral/numeral.cpp
+++ numeral/numeral.cpp
@@ -7,6 +7,7 @@ int main() {
 		std::cout << "@ ";
 		std::getline(std::cin, input);
 		std::vector<lexer::Token*> tokens = (new lexer::Lexer(input))->tokenize();
+		if (tokens.size() == 0) continue;
 		parser::Node* root = (new parser::Parser(tokens))->expr();
 		root->print(0);
 	}
